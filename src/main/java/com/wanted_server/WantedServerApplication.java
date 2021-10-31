19c76a648f245558f4b5faef1b20f91e0f030a96
Hyunseung <ardimento22@naver.com>
2021-11-01 00:27:22 +0900

    origin

---
+++ src/main/java/com/wanted_server/WantedServerApplication.java
@@ -0,0 +1,13 @@
+package com.wanted_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanted_server/WantedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
